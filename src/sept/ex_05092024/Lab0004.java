85dddea3a0c72a4d6507c7d6be5ba65a5b858210
manasisutar1 <manasisutar533@gmail.com>
2024-12-19 19:16:44 +0530

    Changes added

---
+++ src/sept/ex_05092024/Lab0004.java
@@ -0,0 +1,12 @@
+package sept.ex_05092024;
+
+public class Lab0004 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
+}
