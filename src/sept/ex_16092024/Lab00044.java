b6eb53ae1b2caf5dec1d09b31864cf9bb97e5b27
manasisutar1 <manasisutar533@gmail.com>
2024-12-21 13:25:20 +0530

    todays practice 21/12/24

---
+++ src/sept/ex_16092024/Lab00044.java
@@ -0,0 +1,10 @@
+package sept.ex_16092024;
+
+public class Lab00044 {
+    public static void main(String[] args) {
+        short a=10;
+        char b='A';//65
+        int result=a+b;
+        System.out.println(result);
+    }
+}
