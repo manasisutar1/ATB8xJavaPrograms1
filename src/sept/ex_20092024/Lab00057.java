379b0d9ff34680605f0d1d3c26358d037a0a722c
manasisutar1 <manasisutar533@gmail.com>
2025-01-03 21:00:36 +0530

    today practice 3/1/2024 time 9.00 pm

---
+++ src/sept/ex_20092024/Lab00057.java
@@ -0,0 +1,20 @@
+package sept.ex_20092024;
+
+public class Lab00057 {
+
+    public static void main(String[] args) {
+        int score=85;
+        String Grade_A=(score>=90)?"Grade A":"Grade B";
+        String Grade_B=(score>=80)?"Grade B":"Grade C";
+        String Grade_c=(score>=70)?"Grade C":"Grade D";
+        System.out.println(Grade_A);
+        System.out.println(Grade_B);
+        System.out.println(Grade_c);
+
+
+
+
+
+
+    }
+}
