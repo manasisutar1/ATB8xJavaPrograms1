564df09bc09ec18a8b6605fc22e4fd5c4db5b644
manasisutar1 <manasisutar533@gmail.com>
2025-01-03 21:28:35 +0530

    today practice 3/1/2024 time 9.28 pm

---
+++ src/sept/ex_20092024/Lab00058_MaxNumbers.java
@@ -0,0 +1,15 @@
+package sept.ex_20092024;
+
+public class Lab00058_MaxNumbers {
+    public static void main(String[] args) {
+        //Ternary opretors
+        //Max between 3 numbers
+        int a=10,b=20,c=45,max;
+        max=(a>b)?(a>c ? a:c):(b>c? b:c);
+        System.out.println("Maximum number is "+a+"," +b+ "and" +c+ "is" +max);
+
+
+
+            }
+}
+
