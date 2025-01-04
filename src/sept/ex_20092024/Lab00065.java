6d2646785f0b70dc5fafc88a866224a5d52b71cc
manasisutar1 <manasisutar533@gmail.com>
2025-01-04 14:08:49 +0530

    today practice 4/1/2024 time 11.39Am

--- src/sept/ex_20092024/Lab00065.java
+++ src/sept/ex_20092024/Lab00065.java
@@ -2,7 +2,7 @@ package sept.ex_20092024;
 
 public class Lab00065 {
     public static void main(String[] args) {
-        int num1=30;
+        int num1=50;
         int num2=20;
         if(num1 > num2){
             System.out.println("num1");
--- src/sept/ex_23092024/Lab00069.java
+++ src/sept/ex_23092024/Lab00069.java
@@ -2,6 +2,21 @@ package sept.ex_23092024;
 
 public class Lab00069 {
     public static void main(String[] args) {
+        //Web automation
+        //I will ask user to which browser you want to run the code
+        //Chrome->execute chrome
+        //Firefox->execute firefox
+        //Edge ->execute edge
 
+        String browser="Chrome";
+       switch(browser){
+       }
     }
 }
+
+
+
+
+
+
+
