1acf5dcc2db0a2b188cdbab120356a39c401c00b
manasisutar1 <manasisutar533@gmail.com>
2025-01-04 14:11:40 +0530

    today practice 4/1/2024 time 2.10Pm

--- src/sept/ex_20092024/Lab00065.java
+++ src/sept/ex_20092024/Lab00065.java
@@ -3,7 +3,7 @@ package sept.ex_20092024;
 public class Lab00065 {
     public static void main(String[] args) {
         int num1=50;
-        int num2=20;
+        int num2=30;
         if(num1 > num2){
             System.out.println("num1");
         }
