624c9d2b05fb5b3919989d8d857161555781f99e
manasisutar1 <manasisutar533@gmail.com>
2025-01-04 11:43:29 +0530

    today practice 4/1/2024 time 11.39Am

--- src/sept/ex_20092024/Lab00066.java
+++ src/sept/ex_20092024/Lab00066.java
@@ -3,13 +3,13 @@ package sept.ex_20092024;
 public class Lab00066 {
     public static void main(String[] args) {
 
-        int num1=30;
+        int num1=90;
         int num2=30;
         if(num1>num2){
             System.out.println("num1");
         } else if (num2>num1) {
             System.out.println("num2");
-            
+
         }
         else {
             System.out.println("Both values are Equal");
